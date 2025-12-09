9b9ff9414a579352451acc1d63b5ecf49aa81dcd
6704062611239 <s6704062611239@email.kmutnb.ac.th>
2025-12-09 14:49:55 +0700

    calculate VAT

--- sale.c
+++ sale.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 int main(){
-	int n1,n2,n3,sum;
-	scanf("%d",&n1);
+	float n1,n2,n3,sum;
+	scanf("%f",&n1);
 	float N1 = n1*0.05;
-	printf("Promotion A %2f\n",n1-N1);
-	scanf("%d",&n2);
+	printf("Promotion A %.2f\n",n1-N1);
+	scanf("%f",&n2);
 	sum = n1+n2;
 	float N2 = sum*0.15;
-	printf("Promotion B %2f\n",sum-N2);
-	scanf("%d",&n3);
+	printf("Promotion B %.2f\n",sum-N2);
+	scanf("%f",&n3);
 	sum = n1+n2+n3;
 	float N3 = sum*0.3;
-	printf("Promotion C %2f",sum-N3);
-	
+	printf("Promotion C %.2f\n",sum-N3);
+	float x = sum-N3;
+	float v = x*0.07;	
+	printf("VAT 7 = %.2f",x-v);
 }
